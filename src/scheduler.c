ff934c1efeed80f9a76b739161ef9f18f4fabd5c
Magnus Deininger <dma05@web.de>
2009-11-19 12:17:13 +0100

    stubs and infra for mode switching

--- src/scheduler.c
+++ src/scheduler.c
@@ -32,12 +32,27 @@
 #include <kyuba/ipc.h>
 #include <kyuba/types.h>
 
+define_symbol (sym_scheduler,            "scheduler");
+define_symbol (sym_modes,                "modes");
+define_symbol (sym_get_mode,             "get-mode");
+define_symbol (sym_mode,                 "mode");
 define_symbol (sym_update,               "update");
+define_symbol (sym_switch,               "switch");
+define_symbol (sym_switching_to,         "switching-to");
 define_symbol (sym_initialising,         "initialising");
 define_symbol (sym_initialised,          "initialised");
 define_symbol (sym_get_scheduler_data,   "get-scheduler-data");
+define_symbol (sym_default,              "default");
+define_symbol (sym_no_data_for_mode,     "no-data-for-mode");
+define_symbol (sym_reevaluating_mode,    "reevaluating-mode");
 
-static sexpr system_data;
+static sexpr system_data,
+             current_mode         = sx_nonexistent,
+             target_mode          = sx_nonexistent,
+             target_mode_enable   = sx_end_of_list,
+             target_mode_disable  = sx_end_of_list,
+             target_mode_ipc      = sx_end_of_list,
+             mode_specifications  = sx_nonexistent;
 static int currently_initialising = 0;
 
 /* the module list is taken as the primary listing, so this function is
@@ -158,7 +173,7 @@ static void update_services ( void )
     }
 }
 
-static void print_scheduler_data ()
+static void print_scheduler_data ( void )
 {
     sexpr sys = lx_environment_alist (system_data);
 
@@ -170,6 +185,33 @@ static void print_scheduler_data ()
 
         sys = cdr (sys);
     }
+}
+
+static void rebuild_target_data ( void )
+{
+    /* TODO: look up the target mode and merge all enable/disable/ipc data */
+}
+
+static void reevaluate_target_data ( void )
+{
+    kyu_command (cons (sym_reevaluating_mode,
+                       cons (target_mode, sx_end_of_list)));
+
+    rebuild_target_data ();
+
+    /* TODO: do actual mode switching here */
+
+    /* TODO: send this once the new mode is active: */
+/*    current_mode = mode;
+    kyu_command (cons (sym_mode, cons (mode, sx_end_of_list)));*/
+}
+
+static void switch_mode (sexpr mode)
+{
+    kyu_command (cons (sym_switching_to, cons (mode, sx_end_of_list)));
+    target_mode = mode;
+
+    reevaluate_target_data ();
 }
 
 static void on_event (sexpr sx, void *aux)
@@ -269,19 +311,56 @@ static void on_event (sexpr sx, void *aux)
             {
                 update_services();
                 print_scheduler_data ();
+                reevaluate_target_data ();
             }
         }
         else if (truep (equalp (a, sym_get_scheduler_data)))
         {
             print_scheduler_data ();
         }
+        else if (truep (equalp (a, sym_reply)))
+        {
+            sx = cdr (sx);
+            a = car (sx);
+
+            if (truep (equalp (a, sym_configuration)))
+            {
+                sx = cdr (sx);
+                a = car (sx);
+
+                if (truep (equalp (a, sym_modes)))
+                {
+                    a = car (cdr (sx));
+
+                    if (environmentp (a))
+                    {
+                        mode_specifications = a;
+
+                        reevaluate_target_data ();
+                    }
+                }
+            }
+        }
+        else if (truep (equalp (a, sym_get_mode)))
+        {
+            kyu_command (cons (sym_mode, cons (current_mode, sx_end_of_list)));
+        }
+        else if (truep (equalp (a, sym_switch)))
+        {
+            switch_mode (car (cdr (sx)));
+        }
     }
+}
+
+static void read_configuration ()
+{
+        kyu_command (cons (sym_request,
+                           cons (sym_configuration,
+                                 cons (sym_modes, sx_end_of_list))));
 }
 
 int cmain ()
 {
-    define_symbol (sym_scheduler, "scheduler");
-
     terminate_on_allocation_errors ();
 
     programme_identification = cons (sym_scheduler, make_integer (1));
@@ -291,6 +370,9 @@ int cmain ()
     initialise_kyu_script_commands ();
     initialise_kyu_types ();
     multiplex_add_kyu_stdio (on_event, (void *)0);
+
+    read_configuration ();
+    switch_mode (sym_default);
 
     while (multiplex() == mx_ok);
 
